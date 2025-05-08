7068138430e0c9bde64eaffa24c5b7c2192bb313
EmmaJennings <ejennings13@my.bcit.ca>
2025-05-08 14:07:24 -0700

    in process of fixing ingredient cards

--- dermaknight/app/ui/ingredientCard/ingredientCard.js
+++ dermaknight/app/ui/ingredientCard/ingredientCard.js
@@ -1,43 +1,35 @@
 import styles from "./ingredientCard.module.css";
-import Image from "@public/ingredients";
+import Image from "next/image";
 
-const products = [
-    {
-        name: "Niacinamide:",
-        image: "image_1",
-        description: "Helps brighten skin and fade dark marks gently",
-    }, 
-    {
-        name: "Vitamin C:",
-        image: "image_2",
-        description: "Antioxidant that evens out skin tone",
-    }, 
-    {
-        name: "Ceramides:",
-        image: "image_1",
-        description: "Calms irritation and fades post-acne spots",
-    }, 
-    {
-        name: "Retinol:",
-        image: "image_2",
-        description: "A vitamin A derivative that speeds up skin cell turnover.",
-    }, 
-];
+// const ingredients = [
+//     {
+//         name: "Niacinamide:",
+//         image: "/ingredients/image_1.png",
+//         description: "Helps brighten skin and fade dark marks gently",
+//     }, 
+//     {
+//         name: "Vitamin C:",
+//         image: "/ingredients/image_2.png",
+//         description: "Antioxidant that evens out skin tone",
+//     }, 
+//     {
+//         name: "Ceramides:",
+//         image: "/ingredients/image_1.png",
+//         description: "Calms irritation and fades post-acne spots",
+//     }, 
+//     {
+//         name: "Retinol:",
+//         image: "/ingredients/image_2.png",
+//         description: "A vitamin A derivative that speeds up skin cell turnover.",
+//     }, 
+// ];
 
 function Ingredients({ name, image, description }) {
-    let descriptionClasses;
-    let ingredientDescription = description;
-
-    // if(defaultState) {
-    //     ingredientType = styles.recommendedIngredient;
-    //     name = "Niacinamide";
-    // }
-
     return (
         <button>
             <img className={styles.ingredient__image} src={image} />
             <p className={styles.ingredient__name}>{name}</p>
-            <p className={descriptionClasses}>${description}</p>
+            <p className={styles.ingredient_description}>{description}</p>
         </button>
     );
 }
